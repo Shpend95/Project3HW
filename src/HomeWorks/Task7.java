012f17370554b0613c26cc4eaac4a96a1a48bd1e
Shpend95 <shpendpllana5@gmail.com>
2024-03-20 00:22:39 -0400

    first commit

--- src/HomeWorks/Task7.java
+++ src/HomeWorks/Task7.java
@@ -4,5 +4,18 @@ public class Task7 {
     public static void main(String[] args) {
         //	Write a Java Program to print the first 10 numbers of Fibonacci series.
 
+        int num1=0;
+        int num2=1;
+
+        for(int i=0; i<15; i++){
+            System.out.print(num1 + " ");
+            int num3=num2 + num1;
+            num1=num2;
+            num2=num3;
+
+        }
+
+
+
     }
 }
